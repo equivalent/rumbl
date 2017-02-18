4daa9dee12335c30a2264a1c57e880425a8cc20f
Tomas Valent <equivalent@eq8.eu>
2017-02-19 00:55:45 +0100

    registration changeset  with actuall password hash

--- README-steps.ex
+++ README-steps.ex
@@ -13,3 +13,20 @@ jose.name # "Jose Valim"
 
 chris = %User{nmae: "chris"}
 #** (CompileError) iex:3: unknown key :nmae for struct User
+#
+
+
+
+## Changeset
+
+```exs
+ch = User.registration_changeset(%User{}, %{username: "max", name: "Max", password: "123"})
+ch.valid? # false
+
+# update existing records
+for u <- Rumbl.Repo.all(User) do
+  Rumbl.Repo.update!(User.registration_changeset(u, %{password: u.password_hash || "temppass"}))
+end
+```
+
+
