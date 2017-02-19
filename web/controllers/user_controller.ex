6c85c92ee94e8aa44bf07b473bbf3ec01847c41b
Tomas Valent <equivalent@eq8.eu>
2017-02-19 02:34:14 +0100

    session create/destroy

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -24,6 +24,7 @@ defmodule Rumbl.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "User #{user.name} was created")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
